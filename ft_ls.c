577601c445a6bb5d70eb0902b3dda3230dde879d
Simo Puustinen <spuustin@c1r6p19.hive.fi>
2022-04-21 13:58:45 +0300

    created file

---
+++ ft_ls.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spuustin <spuustin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/21 13:26:53 by spuustin          #+#    #+#             */
+/*   Updated: 2022/04/21 13:58:08 by spuustin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Within your mandatory part you are allowed to use the following functions:
+◦ write
+opendir, readdir, closedir 
+◦ stat; lstat
+◦ getpwuid ◦ getgrgid ◦ listxattr ◦ getxattr 
+◦ time, ctime
+◦ readlink 
+◦ malloc, free
+◦ perror, strerror ◦ exit
+
+Amongst the numerous options available, we are asking you to create the
+following: -l, -R, -a, -r and -t.
+We strongly recommend that you account for the implications of the
+option -R from the very beginning of your code...
+*/
+
+#include "ft_ls.h"

ef6e132e69c2dfa1fccddd314f201ae04a6e0cd2
Simo Puustinen <spuustin@c1r6p19.hive.fi>
2022-04-21 17:04:56 +0300

    added files for project

--- ft_ls.c
+++ ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: spuustin <spuustin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 13:26:53 by spuustin          #+#    #+#             */
-/*   Updated: 2022/04/21 13:58:08 by spuustin         ###   ########.fr       */
+/*   Updated: 2022/04/21 16:53:56 by spuustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,43 @@ opendir, readdir, closedir
 ◦ malloc, free
 ◦ perror, strerror ◦ exit
 
+For each operand that names a file of a type other than directory, ls displays its name as well as any
+     requested, associated information.  For each operand that names a file of type directory, ls displays the
+     names of files contained within that directory, as well as any requested, associated information.
+
+     If no operands are given, the contents of the current directory are displayed.  If more than one operand is
+     given, non-directory operands are displayed first; directory and non-directory operands are sorted sepa-
+     rately and in lexicographical order.
+
 Amongst the numerous options available, we are asking you to create the
 following: -l, -R, -a, -r and -t.
 We strongly recommend that you account for the implications of the
 option -R from the very beginning of your code...
+
+You do not have to deal with the multiple column format for the exit when
+the option -l isn’t in the arguments.
+◦ You are not required to deal with ACL and extended attributes.
+◦ The overall display, depending on each option, must stay as identical as pos- sible to the system command. 
+We will be cordial when grading either the padding or the pagination, but no information can be missing.
+
+-l = list in long-format. if the output is to a terminal, a total sum for all
+the file sizes is output on a line before the long listing.
+-R = recursively list subdirectories encountered. !!!
+-a = include directory entries whose names begin with a dot '.'
+-r = reverse the order of the sort to reverse lexicographical order or the 
+oldest entries first (or largest files last, if combined with sort by size)
+-t = sort by time modifie (most recently modified first) before sorting
+the operands by lexicographical order.
 */
 
 #include "ft_ls.h"
+
+int	ft_ls()
+{
+	t_flags *flags;
+
+	flags = (t_flags *)malloc(sizeof(t_flags));
+	if (!flags)
+		exit(-1);
+	return (0);
+}
--- ft_ls.h
+++ ft_ls.h
@@ -6,12 +6,40 @@
 /*   By: spuustin <spuustin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 13:54:59 by spuustin          #+#    #+#             */
-/*   Updated: 2022/04/21 13:55:20 by spuustin         ###   ########.fr       */
+/*   Updated: 2022/04/21 17:03:49 by spuustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 #define FT_LS_H
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <dirent.h>
+//for directory operations (opendir)
+#include <stdio.h>
+//for system error message (perror)
+#include <string.h>
+//for system error message (strerror)
+#include <sys/stat.h>
+//for file status (stat, lstat)
+#include <sys/types.h>
+//for password database operations (getpwuid)
+#include <grp.h>
+#include <uuid/uuid.h>
+//for fatabase operations
+#include <sys/xattr.h>
+//for listxattr/getxattr
+
+
+
+typedef struct s_flags
+{
+	int	l;
+	int r;
+	int R;
+	int a;
+	int t;
+}	t_flags;
 
 #endif

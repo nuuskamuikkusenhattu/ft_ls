9b0a10105e1ce28235d4a0bca4f21da54af69244
Simo Puustinen <puustinen.simo@gmail.com>
2022-08-03 12:35:38 +0300

    added sort_rt (sort.c), doesnt quite work universally tho

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: spuustin <spuustin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 18:58:56 by spuustin          #+#    #+#             */
-/*   Updated: 2022/08/02 12:52:31 by spuustin         ###   ########.fr       */
+/*   Updated: 2022/08/03 12:15:43 by spuustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,6 +79,41 @@ void	sort_time(char **list)
 			temp = list[i];
 			list[i] = list[i + 1];
 			list[i + 1] = temp;
+			i-=2;
+		}
+		i++;
+		if (i < 0)
+			i = 0;
+	}
+}
+
+/*
+if -rt, then dirs need to be sortet first with -t (subdir sensitive)
+and then reversed (subdir sensitive)
+*/
+
+void	sort_rt(t_ls *b)
+{
+	int		i;
+	char	*temp;
+	struct stat f_status;
+	long long	this_time;
+	long long	next_time;
+	sort_ascii(b->dir_list);
+
+	i = 0;
+	while (b->dir_list[i] && b->dir_list[i + 1])
+	{
+		stat(b->dir_list[i], &f_status);
+		this_time = f_status.st_mtime;
+		stat(b->dir_list[i + 1], &f_status);
+		next_time = f_status.st_mtime;
+		if (this_time < next_time && \
+		is_subdir_substr(b->dir_list[i], b->dir_list[i + 1], ft_strlen(b->dir_list[i])) == 0)
+		{
+			temp =b->dir_list[i];
+			b->dir_list[i] = b->dir_list[i + 1];
+			b->dir_list[i + 1] = temp;
 			i-=2;
 		}
 		i++;
